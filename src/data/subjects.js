acf31584fdeda8293c74b42434bb72ccefb32dd2
karla-walker <indiablonix@gmail.com>
2022-10-14 00:15:55 -0700

    Add data for subject info

---
+++ src/data/subjects.js
@@ -0,0 +1,64 @@
+const subjects = [
+  {
+    id: 1,
+    name: "Aliens: Council Races",
+  },
+  {
+    id: 2,
+    name: "Aliens: Extinct Races",
+  },
+  {
+    id: 3,
+    name: "Aliens: Non-Council Races",
+  },
+  {
+    id: 4,
+    name: "Aliens: Non-Sapient Creatures",
+  },
+  {
+    id: 5,
+    name: "Citadel and Galactic Government",
+  },
+  {
+    id: 6,
+    name: "Humanity and the Systems Alliance",
+  },
+  {
+    id: 7,
+    name: "Known Associates",
+  },
+  {
+    id: 8,
+    name: "Organizations",
+  },
+  {
+    id: 9,
+    name: "Personal History Summary",
+  },
+  {
+    id: 10,
+    name: "Planets and Locations",
+  },
+  {
+    id: 11,
+    name: "Publications",
+  },
+  {
+    id: 12,
+    name: "Ships and Vehicles",
+  },
+  {
+    id: 13,
+    name: "Technology",
+  },
+  {
+    id: 14,
+    name: "The Reapers",
+  },
+  {
+    id: 15,
+    name: "Weapons, Armor and Equipment",
+  },
+]
+
+export default subjects
