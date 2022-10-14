2092e21ad2fb9d3dc154c7beb9db2f1ee60a640f
karla-walker <indiablonix@gmail.com>
2022-10-14 00:17:20 -0700

    Add component for website header

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h1>Mass Effect Codex</h1>
+        <p>
+          Welcome to my Mass Effect Codex project! Here you’ll find codex entries from all three
+          Mass Effect games (minus the entries from the DLCs).
+        </p>
+        <p>
+          All codex entries are placed into one of two categories: Primary and Secondary.
+          **Primary** entries consists of blank subcategories that define different areas of
+          interest that are key to the storyline of Mass Effect. **Secondary** entries consist of
+          blank subcategories that dive deeper into areas that are defined in the Primary entries.
+        </p>
+      </div>
+      <div>
+        <p>Sort by:</p>
+        <ul>
+          <li>
+            <button>Category</button>
+          </li>
+          <li>
+            <button>Subject</button>
+          </li>
+        </ul>
+      </div>
+    </header>
+  )
+}
+
+export default Header
