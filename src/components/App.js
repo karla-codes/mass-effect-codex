2e72b51092b87da2b31645f76dad1e1231665c14
karla-walker <indiablonix@gmail.com>
2022-10-14 00:15:00 -0700

    Add fetch request for codex entries and a function that divides entries by subject

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,55 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import subjects from "../data/subjects"
+import Header from "./Header"
+import Entries from "./Entries"
 
-function App() {
-  return <h1>Hello World!</h1>
+const App = () => {
+  const [allEntries, setAllEntries] = useState([])
+  const [bySubjectEntries, setBySubjectEntries] = useState([])
+  // const [byCategoryEntries, setByCategoryEntries] = useState([])
+
+  // display ALL entries in alphabetical order
+
+  // TODO: Divide all entries and categorize by subject
+  const organizeEntriesBySubject = () => {
+    bySubjectEntries.map(subject => {
+      const subjectId = subject.id
+
+      const subjectEntries = allEntries.filter(entry => entry.subject._id === subjectId)
+
+      const updatedSubject = {
+        ...subject,
+        subjectEntries,
+      }
+
+      return updatedSubject
+    })
+  }
+
+  organizeEntriesBySubject()
+  // TODO: display ALL entries divided by Category (a list of category objects?)
+  // in alphabetical order
+  // const displayByCategory = () => {}
+
+  useEffect(() => {
+    // GET all entries
+    fetch("http://localhost:5500/api/entries/all")
+      .then(res => res.json())
+      .then(data => {
+        setAllEntries(data)
+        setBySubjectEntries(subjects)
+      })
+      .catch(err => console.log(err.message))
+  }, [])
+
+  // console.log(bySubjectEntries)
+
+  return (
+    <div>
+      <Header />
+      <Entries entries={allEntries} />
+    </div>
+  )
 }
 
 export default App
