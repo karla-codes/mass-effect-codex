eaf531627a848ee055848f9a0a669584ff50ce9e
karla-walker <indiablonix@gmail.com>
2022-10-16 22:20:15 -0700

    Update entries function to organize entries by subject and set state for all entries/entries by subject

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import subjects from "../data/subjects"
 import Header from "./Header"
+import Search from "./Search"
 import Entries from "./Entries"
 
 const App = () => {
@@ -11,11 +12,11 @@ const App = () => {
   // display ALL entries in alphabetical order
 
   // TODO: Divide all entries and categorize by subject
-  const organizeEntriesBySubject = () => {
-    bySubjectEntries.map(subject => {
+  const organizeEntries = (entries, listOfSubjects) => {
+    const updatedSubjects = listOfSubjects.map(subject => {
       const subjectId = subject.id
 
-      const subjectEntries = allEntries.filter(entry => entry.subject._id === subjectId)
+      const subjectEntries = entries.filter(entry => entry.subject._id === subjectId)
 
       const updatedSubject = {
         ...subject,
@@ -24,30 +25,25 @@ const App = () => {
 
       return updatedSubject
     })
+    setAllEntries(entries)
+    setBySubjectEntries(updatedSubjects)
   }
-
-  organizeEntriesBySubject()
-  // TODO: display ALL entries divided by Category (a list of category objects?)
-  // in alphabetical order
-  // const displayByCategory = () => {}
 
   useEffect(() => {
     // GET all entries
     fetch("http://localhost:5500/api/entries/all")
       .then(res => res.json())
       .then(data => {
-        setAllEntries(data)
-        setBySubjectEntries(subjects)
+        organizeEntries(data, subjects)
       })
       .catch(err => console.log(err.message))
   }, [])
 
-  // console.log(bySubjectEntries)
-
   return (
     <div>
       <Header />
-      <Entries entries={allEntries} />
+      <Search />
+      <Entries allEntries={allEntries} bySubjectEntries={bySubjectEntries} />
     </div>
   )
 }
