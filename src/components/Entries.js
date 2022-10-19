3ff7fb3210daef7c52d2cd9cf5b68fde218a1b0f
karla-walker <indiablonix@gmail.com>
2022-10-19 16:53:07 -0700

    Add function that sets order of current entries based on the sort selection

--- src/components/Entries.js
+++ src/components/Entries.js
@@ -1,35 +1,150 @@
-import React from "react"
+import React, { useState } from "react"
+import EntryCard from "./EntryCard"
 
 const Entries = props => {
+  const [selectionValue, setSelectionValue] = useState("")
+  const [currentEntries, setCurrentEntries] = useState([])
   const entries = props.allEntries
-  // props.organizeEntriesBySubject(entries)
+  const { subjects } = props
+
+  const setEntryOrder = option => {
+    if (option === "all-atoz") {
+      // display ALL entries in alphabetical order
+      const sortedAtoZ = entries.sort((a, b) => {
+        if (a.title < b.title) {
+          return -1
+        }
+
+        if (a.title > b.title) {
+          return 1
+        }
+
+        return 0
+      })
+
+      setCurrentEntries(sortedAtoZ)
+    } else if (option === "all-ztoa") {
+      // display ALL entries in reverse alphabetical order
+      const sortedZtoA = entries.sort((a, b) => {
+        if (a.title > b.title) {
+          return -1
+        }
+
+        if (a.title < b.title) {
+          return 1
+        }
+
+        return 0
+      })
+
+      setCurrentEntries(sortedZtoA)
+    } else if (option === "category-first" || option === "category-last") {
+      // display all entries by Category (first - last)
+      const primaryEntries = []
+      const secondaryEntries = []
+
+      entries.forEach(entry => {
+        if (entry.category.name === "Primary") {
+          primaryEntries.push(entry)
+        } else if (entry.category.name === "Secondary") {
+          secondaryEntries.push(entry)
+        }
+      })
+
+      if (option === "category-first") {
+        const sortedCategoryFirst = [primaryEntries, secondaryEntries]
+
+        sortedCategoryFirst.forEach(category => {
+          category.sort((a, b) => {
+            if (a.title < b.title) {
+              return -1
+            }
+            if (a.title > b.title) {
+              return 1
+            }
+
+            return 0
+          })
+        })
+
+        setCurrentEntries(sortedCategoryFirst)
+      } else {
+        const sortedCategoryLast = [secondaryEntries, primaryEntries]
+
+        sortedCategoryLast.forEach(category => {
+          category.sort((a, b) => {
+            if (a.title < b.title) {
+              return -1
+            }
+            if (a.title > b.title) {
+              return 1
+            }
+
+            return 0
+          })
+        })
+
+        setCurrentEntries(sortedCategoryLast)
+      }
+    } else if (option === "subject-atoz") {
+      // display all entries by Subject (a - z)
+      subjects.sort((a, b) => {
+        if (a.name < b.name) {
+          return -1
+        }
+
+        if (a.name > b.name) {
+          return 1
+        }
+
+        return 0
+      })
+
+      setCurrentEntries(subjects)
+    } else if (option === "subject-ztoa") {
+      // display all entries by Subject (z - a)
+      subjects.sort((a, b) => {
+        if (a.name > b.name) {
+          return -1
+        }
+
+        if (a.name < b.name) {
+          return 1
+        }
+
+        return 0
+      })
+
+      setCurrentEntries(subjects)
+    }
+  }
+
+  const handleOptionSelect = e => {
+    const selectValue = e.target.value
+    setSelectionValue(selectValue)
+    setEntryOrder(selectValue)
+    // console.log(selectValue)
+  }
 
   return (
     <main>
       This is where all of the entries will exist.
-      <div className="container">
+      <div className="wrapper">
         <div className="sort-select">
           {/* TODO: This is where the 'sort by' selector will go */}
           <label htmlFor="sort-select">Sort By</label>
-          <select id="sort-select">
-            <option value={"atoz"}>A - Z</option>
-            <option value={"ztoa"}>Z - A</option>
-            <option value={"subject"}>Subject (A - Z)</option>
+          <select id="sort-select" value={selectionValue} onChange={handleOptionSelect}>
+            <option value={"all-atoz"}>All (a - z)</option>
+            <option value={"all-ztoa"}>All (z - a)</option>
+            <option value={"category-first"}>Category (First)</option>
+            <option value={"category-last"}>Category (Last)</option>
+            <option value={"subject-atoz"}>Subject (a - z)</option>
+            <option value={"subject-ztoa"}>Subject (z - a)</option>
           </select>
         </div>
-        <div className="entry-cards">
-          {entries.map(entry => {
-            return (
-              <div key={entry._id}>
-                <h2>{entry.title}</h2>
-                {/* <div>{entry.content.map(item => item)}</div> */}
-                <p>
-                  {entry.subject.name} | {entry.category.name}
-                </p>
-              </div>
-            )
-          })}
-        </div>
+        <article className="entries">
+          <EntryCard entries={entries} updatedEntries={currentEntries} />
+        </article>
       </div>
     </main>
   )
