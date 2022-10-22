4d50c7518f3d5cb9c2d7c1e1f3e3ed796b9eabaf
karla-walker <indiablonix@gmail.com>
2022-10-21 23:33:34 -0700

    Update css location

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import "./css/index.css"
+import "./sass/style.scss"
 import App from "./components/App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
