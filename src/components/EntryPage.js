9419d53f366b9a10f757137056b20de4277e9fd0
karla-walker <indiablonix@gmail.com>
2022-11-07 19:44:43 -0800

    Add button that navigates to previous in history and add scroll to top when navigating to entry page

--- src/components/EntryPage.js
+++ src/components/EntryPage.js
@@ -1,33 +1,44 @@
-import React from "react"
-import { useLocation } from "react-router-dom"
+import React, { useEffect } from "react"
+import { useLocation, useNavigate } from "react-router-dom"
 
 const EntryPage = () => {
   const location = useLocation()
   const currentEntry = location.state?.entry
   const entryContent = currentEntry.content
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
 
   return (
-    <main>
-      <article className="codex-entry__info">
-        <header>
-          <nav>
-            <ul>
-              <li>{/* <a href="">Previous Entry</a> */}</li>
-              <li>{/* <a href="">Next Entry</a> */}</li>
-            </ul>
-          </nav>
+    <div className="wrapper">
+      <nav className="entry-nav">
+        <ul>
+          <li>
+            <button onClick={() => navigate(-1)}>Back to Entries</button>
+          </li>
+          <li>
+            <button>Random Entry</button>
+          </li>
+        </ul>
+      </nav>
+      <div className="entry-container">
+        <header id="entry-page">
           <h1>{currentEntry.title}</h1>
           <p>
             {currentEntry.subject.name} | {currentEntry.category.name}
           </p>
         </header>
-        <div>
-          {entryContent.map((item, i) => (
-            <p key={i}>{item}</p>
-          ))}
-        </div>
-      </article>
-    </main>
+        <main>
+          <article className="entry-info">
+            {entryContent.map((item, i) => (
+              <p key={i}>{item}</p>
+            ))}
+          </article>
+        </main>
+      </div>
+    </div>
   )
 }
 
