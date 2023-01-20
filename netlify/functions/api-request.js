f5157b5df923b9e4cea28ee829e57038ebdf651b
karla-walker <indiablonix@gmail.com>
2023-01-20 14:44:34 -0800

    Update api request

--- netlify/functions/api-request.js
+++ netlify/functions/api-request.js
@@ -1,8 +1,23 @@
+const fetch = require("node-fetch")
+const API_URL = `${process.env.REACT_APP_API_URL}/api/entries/all`
+
 exports.handler = async () => {
-  const data = fetch(`${process.env.REACT_APP_API_URL}/api/entries/all`).then(res => res.json())
+  let response
+  try {
+    response = await fetch(API_URL)
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message,
+      }),
+    }
+  }
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data),
+    body: JSON.stringify({
+      data: response,
+    }),
   }
 }
